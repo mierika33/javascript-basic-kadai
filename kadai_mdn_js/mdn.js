2ed48835f565f88ad0dd10653f3eacc7bdd119dc
mierika33 <evergreen.bishin@gmail.com>
2025-11-07 15:17:56 +0900

    kadai_mdn_jsファイルをアップロード

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,8 @@
+const today = new Date();
+const fullYear = today.getFullYear();
+const moonLanding = today.getMonth() + 1;
+const day = today.getDate();
+
+console.log(fullYear +'年' + moonLanding + '月' + day + '日');
+
+
